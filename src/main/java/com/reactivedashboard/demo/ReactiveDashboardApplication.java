4e7a2391d04d85335f4fedfb3cd3c839936ae284
max <24398539+baylism@users.noreply.github.com>
2018-06-19 17:35:15 +0100

    initial commit

---
+++ src/main/java/com/reactivedashboard/demo/ReactiveDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.reactivedashboard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveDashboardApplication.class, args);
+    }
+}
